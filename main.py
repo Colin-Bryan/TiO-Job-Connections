09b4a44dc775011373a665e5f016e320b4a0b979
Colin-Bryan <40872632+Colin-Bryan@users.noreply.github.com>
2022-07-23 09:54:41 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -9,23 +9,31 @@ import time
 from scripts.process_text import ExtractResumeText, AnalyzeText
 from scripts.employer_postings import get_employer_postings, process_URL_postings
 
-# Define main function
 def main():
+    '''
+    Main function to run the Streamlit user interface.
+
+    Users have the option of uploading a resume to compare to job postings, 
+    viewing existing job postings, or updating the job postings database to get
+    new features for comparison to resumes.
+    '''
+
     # Create page config
     st.set_page_config(page_title="Job Connections", page_icon =":handshake:")#,layout="wide")
 
     # Streamlit title 
     st.title(":handshake: Find Job Opportunities")
 
-    # Initilalize form holder as empty element
-    upload_form_holder = st.empty()
+    # Initilalize resume form holder as empty element
+    resume_form_holder = st.empty()
 
     # Initialize Empty Element to hold Screen Content
     screen_content_holder = st.empty()
 
     # Initialize form to hold file uploader and submit button
-    with upload_form_holder.form('file-upload', clear_on_submit=True):
-        uploaded_file = st.file_uploader("Upload your resume - Microsoft Word (.docx) format preferred", type=["docx","PDF"], accept_multiple_files = False)
+    with resume_form_holder.form('file-upload', clear_on_submit=True):
+        st.subheader('📊 Upload a resume to compare to active job postings')
+        uploaded_file = st.file_uploader("Microsoft Word (.DOCX) format preferred", type=["docx","PDF"], accept_multiple_files = False)
         submitted = st.form_submit_button("Process")
 
     # Initialize AnalyzeText class
@@ -39,7 +47,7 @@ def main():
     elif submitted and uploaded_file is not None:
 
         # Hide form
-        upload_form_holder.empty()
+        resume_form_holder.empty()
 
         # First, Scrape Resume and Extract Text
         with screen_content_holder.container():
@@ -101,14 +109,14 @@ def main():
                 # Specifying data type as resume
                 # Uploaded file = resume
                 # Since it is a resume, it returns the resume features
-                word_sim_results_df = anlyz_txt.build_and_analyze_word_count_features(data = [processed_text], jobs_df = jobs_df, data_type='resume', name = name)
+                word_sim_results_df = anlyz_txt.build_and_analyze_word_count_features(data = [processed_text], jobs_df = jobs_df, data_type='resume')
 
                 ### Build and Analyze Resume Features with Sentence Transformer ###
-                sent_trans_sim_df = anlyz_txt.analyze_with_transformer(resume_data = raw_text, jobs_df = jobs_df, data_type='resume')
+                sent_trans_sim_df = anlyz_txt.analyze_with_transformer(resume_data = raw_text, jobs_df = jobs_df, data_type='resume', name = name)
 
                 # Display success message
                 st.success(':smile: Matches found in employer database')
-                st.subheader('Best Matches for {}:'.format(name))
+                st.subheader('Top 10 Matches for {}:'.format(name))
 
                 # Join dataframes
                 display_df = word_sim_results_df.merge(sent_trans_sim_df, left_on = ['Title'], right_on = ['Title'])
@@ -116,14 +124,18 @@ def main():
                 # Calculate average of tf-idf, BoW, and Sentence Transformer similarities
                 display_df['Average Score'] = display_df[['Tf-idf Score', 'BoW Score', 'Transformer Score']].mean(axis = 1)
 
-                # Display dataframe
-                st.dataframe(display_df.sort_values(by = ['Average Score'], ascending = False))
+                # Display dataframe with top 10 matches and select columns
+                st.dataframe(display_df.loc[:9,
+                                            ['Employer','Title','Tf-idf Score','BoW Score','Transformer Score','Average Score']
+                                            ].sort_values(by = ['Average Score'], ascending = False).reset_index(drop=True))
+
+                # Output to JSON format for TiO processing
+                anlyz_txt.output_to_JSON(name, display_df)
                
-                #st.dataframe(sent_trans_df.sort_values(by = ['Transformer Score'], ascending = False))
 
     ######## ---- INITIALIZE SIDEBAR ---- ########
     # Sidebar Image
-    st.sidebar.image('data\images\TiO Logo.png')
+    st.sidebar.image('data\\images\\TiO Logo.png')
 
     # Create Sidebar Title
     st.sidebar.title("Admin Settings")            
@@ -135,42 +147,43 @@ def main():
     # Create View Job Postings Button
     if st.sidebar.button('View Job Postings'):
         # Hide upload form
-        upload_form_holder.empty()
+        resume_form_holder.empty()
 
         # Load in job postings existing in data folder to a dataframe
-        if os.path.exists("data//postings//Job Postings.xlsx"):
-            jobs_df = pd.read_excel("data//postings//Job Postings.xlsx")
+        if os.path.exists("data\\postings\\Job Postings.xlsx"):
+            jobs_df = pd.read_excel("data\\postings\\Job Postings.xlsx")
 
             # Replace holder element with container to show current postings
             with screen_content_holder.container():
+                # Display current postings in database count
                 st.subheader('Current Job Postings in Database: {}'.format(len(jobs_df)))
 
-                # Display sorted dataframe.. for some reason ascending isn't working correctly
-                st.dataframe(jobs_df.loc[:,['Title']].sort_values(by = ['Title'], ascending = True))
+                # Display dataframe
+                st.dataframe(jobs_df.loc[:,['Employer','Title']].sort_values(by = ['Employer'], ascending = True))
 
         else:
             # Show error message
             screen_content_holder.error('Job data does not exist. Update job postings')
-
             
     # Create Update Job Postings Button
     if st.sidebar.button('Update Job Postings'):
 
-        # Hide upload form
-        upload_form_holder.empty()
+        # # Hide upload form
+        resume_form_holder.empty()
 
         # Return list of posts as a dataframe and list of URLs to go scrape the web
         with screen_content_holder.container():
+
             with st.spinner("Updating Job Postings..."):
 
-                # Save URLs from employer postings function to be processed
-                url_list = get_employer_postings()
-
+                # Process employer postings
+                postings_df = get_employer_postings()
+ 
             with st.spinner("Scraping job data from URLs..."):
 
                 # CB 7.16 - Only scraping indeed postings as part of this project. Update to be more comprehensive in future
                 # Processes and tokenizes URL postings
-                jobs_df = process_URL_postings(url_list)
+                jobs_df = process_URL_postings(postings_df)
 
             with st.spinner("Building Word Count Features..."):
                 ### Build word count features from processed_text (Tf-idf and Bag-of-words) ###
@@ -188,7 +201,9 @@ def main():
 
                 # When processing is finished, show job postings with tokenized text
                 st.subheader('Current Job Postings in Database: {}'.format(len(jobs_df)))
-                st.dataframe(jobs_df.loc[:,['Title']].sort_values(by = ['Title'], ascending = True))
+
+                # Display dataframe
+                st.dataframe(jobs_df.loc[:,['Employer','Title']].sort_values(by = ['Employer'], ascending = True))
 
 # Execute main function
 if __name__ == "__main__":
